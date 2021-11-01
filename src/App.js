386f3b8dc17b0a59ffc295cc954db35b090baa75
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-11-01 12:59:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "./App.css";
+import Register from "./Register"
+import Services from "./Services";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Login from "./Login";
+import {Switch,Route,Redirect} from 'react-router-dom';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Services" component={Services} />
+        <Route exact path="/Register" component={Register} />
+        <Route exact path="/Login" component={Login} />
+        <Redirect to="/" />
+      </Switch>
+    </>
+  )
 }
 
-export default App;
+export default App
