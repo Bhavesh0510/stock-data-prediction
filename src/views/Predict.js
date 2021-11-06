578d6609908e06d774827d8d1807c457e2e4d939
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-11-06 19:22:12 +0530

    API for Prediction Added

--- src/views/Predict.js
+++ src/views/Predict.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import "./index.css";
+import "C:/Users/HP/Desktop/ReactStock/stock-data-prediction/node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
 
 function Predict() {
+
+    const onSubmit = () => {
+
+        try {
+            const res1 = await fetch(`https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxCompanySearch.jsp?search=${name}`);
+            const data1 = await res1.json();
+            console.log(data1.data);
+            setdata1(data1.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getStockData();
+        getGainers();
+    }, [])
+
+
+
     return (
-        <div>
-            Prediction
-        </div>
+        <>
+            <form className="form-horizontal" onSubmit={onSubmit}>
+                <input type="text" className="inp" placeholder="Enter Stock name" name="stockname" />
+                <br />
+                <br />
+                <button type="button" name="search" className="btn btn-info" id="btn1">Predict</button>
+            </form>
+        </>
     )
 }
 
